7cb9908290b198a50fb91ee86bcf44ab56784f12
Clay Bennington <claymation296@gmail.com>
2020-06-15 12:36:25 -0600

    Updated to new theming system which handles text contrast seperately for each --app-??-color. Added missing deps. Updated deps. Removed lazy import for paper-fab to simplify webpack build and future maintenance as it was unnecessary. Updated old --spriteful-ease to new --custom-ease and added ease-out fallback. Added comments to js file.

--- scroll-fab.js
+++ scroll-fab.js
@@ -1,11 +1,28 @@
 
-import {
-  AppElement, 
-  html
-}                 from '@longlost/app-element/app-element.js';
-import {listen}   from '@longlost/utils/utils.js';
-import htmlString from './scroll-fab.html';
+/**
+  * `scroll-fab`
+  * 
+  *   A paper-fab that enters when user scrolls down on page height or more, 
+  *   and exits when page is scrolled up less than one page height.
+  *
+  *   Scrolls window to top on click.
+  *
+  *
+  *
+  *
+  *   @customElement
+  *   @polymer
+  *   @demo demo/index.html
+  *
+  *
+  **/
+
+
+import {AppElement, html} from '@longlost/app-element/app-element.js';
+import {listen, unlisten} from '@longlost/utils/utils.js';
+import htmlString         from './scroll-fab.html';
 import '@longlost/app-icons/app-icons.js';
+import '@polymer/paper-fab/paper-fab.js';
 
 
 class ScrollFab extends AppElement {
@@ -16,6 +33,15 @@ class ScrollFab extends AppElement {
   }
 
 
+  static get properties() {
+    return {
+
+      _scrollListenerKey: Object
+
+    };
+  }
+
+
   connectedCallback() {
     super.connectedCallback();
     
@@ -23,34 +49,46 @@ class ScrollFab extends AppElement {
   }
 
 
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    unlisten(this._scrollListenerKey);
+  }
+
+
   __startFabAnimationControl() {
-    let ticking  = false; // perf
+    let ticking  = false; // Perf.
     const height = window.innerHeight;
 
     const toggleFabClass = () => {
       if (ticking) { return; }
+
       ticking = true;
-      window.requestAnimationFrame(async () => {
+
+      window.requestAnimationFrame(() => {
+
         if (window.scrollY > height) {
+
           if (!this.$.fab.classList.contains('fab-entry')) {
-            await import('@polymer/paper-fab/paper-fab.js');
             this.$.fab.classList.add('fab-entry');
           }
         }
         else if (this.$.fab.classList.contains('fab-entry')) {
           this.$.fab.classList.remove('fab-entry');
         }
+
         ticking = false;
       });
     };
 
-    listen(document, 'scroll', toggleFabClass);
+    this._scrollListenerKey = listen(document, 'scroll', toggleFabClass);
   }
 
 
   async __scrollToTopFabClicked() {
     try {
       await this.clicked();
+
       window.scrollTo({behavior: 'smooth', top: 0});
     }
     catch (error) {
